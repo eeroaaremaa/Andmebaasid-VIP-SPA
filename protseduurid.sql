07fe0e325071bc79cd36ff1e6a5dd21b8fd1a8fd
eeroaaremaa <eero.aaremaa@gmail.com>
2020-05-10 17:55:03 +0300

    Tubade kuvamise võimalus lisatud

--- protseduurid.sql
+++ protseduurid.sql
@@ -1,7 +1,8 @@
-
+// Protseduurid 
+// Autorid: Eero Ääremaa ja Oskar Lui Veelma
+// Kasutasime: SQL Central 17.0.4.2053
 
 // Protseduur et arvutada kokku ühe kliendi poolt teenuste kogusumma, mida ta SPAs kulutas
-//DROP PROCEDURE KliendiPooltKulutatudSummaSPAs;
 
 CREATE PROCEDURE KliendiPooltKulutatudSummaSPAs @KliendiID int
 AS
@@ -16,7 +17,6 @@ EXEC KliendiPooltKulutatudSummaSPAs @KliendiID = 1;
 
 
 // Milliseid tube üks teenindaja teenindab ning näitame, kas tegemist on kuldkliendiga
-DROP PROCEDURE TeenindajaToad;
 
 CREATE PROCEDURE TeenindajaToad @TeenindajaID int
 AS 
@@ -34,8 +34,6 @@ EXEC TeenindajaToad @TeenindajaID = 3;
 
 
 // Valime kõik toad, mis on hõivatud ja näitame kliendi nime, isikukoodi, kuldkliendi staatuse, teenindaja nime ja millal tuba vabaneb
-DROP PROCEDURE TubadeÜlevaade;
-
 
 CREATE PROCEDURE TubadeÜlevaade
 AS
@@ -57,8 +55,6 @@ EXEC TubadeÜlevaade;
 // Protseduur kuldklientide leidmiseks, kes on enda kümnende külastuse juures
 // Kui kuldkliendi külastused jaguvad kümnega siis näitame kuldkliendi külastuste summat, vastasel juhul ei näita midagi
 
-// DROP PROCEDURE KuldklientideKampaania;
-
 CREATE PROCEDURE KuldklientideKampaania @KuldkliendiID integer AS
 BEGIN 
 SELECT Külastused_kokku
@@ -69,3 +65,21 @@ END
 GO;
 
 EXEC KuldklientideKampaania @KuldkliendiID = 3;
+
+
+// Protseduur tubade reklaamimiseks ning ühe SPA kõikide erinevate tubade näitamiseks
+// Seda kasutada siis, kui klient helistab ja tahab teada millised toad majas on
+// Lisaks sellele saab seda kasutada siis kui SPAd reklaamida, kui on soovi kõikidest erinevatest tubadest ülevaade anda
+
+CREATE PROCEDURE KõikToad @SPA_ID int AS
+BEGIN 
+SELECT DISTINCT Toad.Toa_nr AS 'Toa number', ROUND(Toad.Yhe_öö_hind, 2) AS 'Ühe öö hind eurodes', Toa_tüübid.Nimetus , 
+Toa_tüübid.Yhekohalised_voodid AS 'Ühekohaliste voodite arv',
+Toa_tüübid.Kahekohalised_voodid AS 'Kahekohaliste voodite arv', Toa_tüübid.Lisavoodi_võimalus AS 'Lisavoodi võimalus'
+FROM Toad, Toa_tüübid
+WHERE Toa_tüübid.Spa_id = @SPA_ID
+AND Toa_tüübid.Toa_tüüp_id = Toad.Toa_tüüp
+END
+GO;
+
+EXEC KõikToad @SPA_ID = 1;
