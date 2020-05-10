27757614a2eeb4344c5332b0faa12486678fa54e
eeroaaremaa <eero.aaremaa@gmail.com>
2020-05-10 16:54:33 +0300

    Teenindajate leidmise ja tubade ülevaate protseduurid loodud

--- protseduurid.sql
+++ protseduurid.sql
@@ -1,8 +1,7 @@
 
 
 // Protseduur et arvutada kokku ühe kliendi poolt teenuste kogusumma, mida ta SPAs kulutas
-
-DROP PROCEDURE KliendiPooltKulutatudSummaSPAs;
+//DROP PROCEDURE KliendiPooltKulutatudSummaSPAs;
 
 CREATE PROCEDURE KliendiPooltKulutatudSummaSPAs @KliendiID int
 AS
@@ -11,7 +10,45 @@ SELECT SUM(Teenused.Teenuse_hind) as SUMMAKOKKU
 FROM Teenused , "Külastaja teenused" 
 WHERE "Külastaja teenused".Teenuse_id=Teenused.Teenus_tüüp_id AND Külastaja_id = @KliendiID
 END
-
 GO;
 
-EXEC KliendiPooltKulutatudSummaSPAs @KliendiID = 4;
+EXEC KliendiPooltKulutatudSummaSPAs @KliendiID = 1;
+
+
+// Milliseid tube üks teenindaja teenindab ning näitame, kas tegemist on kuldkliendiga
+DROP PROCEDURE TeenindajaToad;
+
+CREATE PROCEDURE TeenindajaToad @TeenindajaID int
+AS 
+BEGIN 
+SELECT DISTINCT Ööbivad_kliendid.Toa_nr, Külastajad.Kuldklent
+FROM Teenindajad, Külastajad, Ööbivad_kliendid 
+WHERE 
+    Ööbivad_kliendid.Id = Külastajad.Id
+    AND
+    Külastajad.Teenindaja_id = @TeenindajaID
+END 
+GO;
+
+EXEC TeenindajaToad @TeenindajaID = 3;
+
+
+// Valime kõik toad, mis on hõivatud ja näitame kliendi nime, isikukoodi, kuldkliendi staatuse, teenindaja nime ja millal tuba vabaneb
+DROP PROCEDURE TubadeÜlevaade;
+
+
+CREATE PROCEDURE TubadeÜlevaade
+AS
+BEGIN
+SELECT DISTINCT Külastajad.Eesnimi, Külastajad.Perenimi, 
+Külastajad.Isikukood, Külastajad.Kuldklent, 
+Ööbivad_kliendid.Toa_nr, Teenindajad.Eesnimi, Teenindajad.Perenimi, Ööbivad_kliendid.Väljumine
+FROM Teenindajad, Külastajad, Ööbivad_kliendid 
+WHERE 
+    Ööbivad_kliendid.Id = Külastajad.Id
+AND
+    Külastajad.Teenindaja_id = Teenindajad.Teenindaja_id
+END 
+GO;
+
+EXEC TubadeÜlevaade;
